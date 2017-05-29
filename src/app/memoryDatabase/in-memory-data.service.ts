8a5b3809a5e3cec6b4b5e72ca5c37a9dc2237254
Bartosz-D3V <nix.noxi379@gmail.com>
2017-05-29 15:41:38 +0100

    Button to add new notes added

--- src/app/memoryDatabase/in-memory-data.service.ts
+++ src/app/memoryDatabase/in-memory-data.service.ts
@@ -5,8 +5,6 @@ export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     const notes: [Note] = [
       new Note(1, 'Task of the day!', 'Buy a milk.', true),
-      new Note(1, 'Task of the day!', 'Buy a milk.', true),
-      new Note(1, 'Task of the day!', 'Buy a milk.', true),
       new Note(2, 'If I will find time...', 'Go & buy groceries.', false)
     ];
     return {notes};
