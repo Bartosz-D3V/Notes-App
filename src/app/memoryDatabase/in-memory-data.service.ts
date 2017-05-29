839ef422fb216d57e04632a20e37f9a2d2f9d636
Bartosz-D3V <nix.noxi379@gmail.com>
2017-05-29 15:27:19 +0100

    Note layout added + rwd + icons

--- src/app/memoryDatabase/in-memory-data.service.ts
+++ src/app/memoryDatabase/in-memory-data.service.ts
@@ -5,6 +5,8 @@ export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     const notes: [Note] = [
       new Note(1, 'Task of the day!', 'Buy a milk.', true),
+      new Note(1, 'Task of the day!', 'Buy a milk.', true),
+      new Note(1, 'Task of the day!', 'Buy a milk.', true),
       new Note(2, 'If I will find time...', 'Go & buy groceries.', false)
     ];
     return {notes};
