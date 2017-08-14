4a17c09ae1750c14928ca3b1114f691089087263
Bartosz-D3V <nix.noxi379@gmail.com>
2017-08-14 21:23:17 +0100

    style(root): improved RVD

--- src/app/notes/notes-list/notes-list.component.ts
+++ src/app/notes/notes-list/notes-list.component.ts
@@ -5,6 +5,7 @@ import { NoteService } from '../notes-service/note.service';
 @Component({
   selector: 'app-notes-list',
   templateUrl: './notes-list.component.html',
+  styleUrls: ['./notes-list.component.scss'],
 })
 export class NotesListComponent implements OnInit {
 
