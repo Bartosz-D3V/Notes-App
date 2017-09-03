7450d104084957cbe5d5d390830674c94745a817
Bartosz-D3V <nix.noxi379@gmail.com>
2017-09-03 19:50:10 +0100

    test(note-list-component): implemented all unit tests

--- src/app/notes/notes-list/notes-list.component.spec.ts
+++ src/app/notes/notes-list/notes-list.component.spec.ts
@@ -96,7 +96,7 @@ describe('NotesListComponent', () => {
       spyOn(noteListComponent, 'getStarredNotes');
       spy = chai.spy.on(noteListComponent, 'getStarredNotes');
       noteListComponent.filter = 'starred';
-      noteListComponent.getStarredNotes();
+      noteListComponent.retrieveNotes();
       expect(spy).to.have.been.called.once;
     });
 
@@ -104,7 +104,7 @@ describe('NotesListComponent', () => {
       spyOn(noteListComponent, 'getDoneNotes');
       spy = chai.spy.on(noteListComponent, 'getDoneNotes');
       noteListComponent.filter = 'done';
-      noteListComponent.getDoneNotes();
+      noteListComponent.retrieveNotes();
       expect(spy).to.have.been.called.once;
     });
 
@@ -112,7 +112,7 @@ describe('NotesListComponent', () => {
       spyOn(noteListComponent, 'getDeletedNotes');
       spy = chai.spy.on(noteListComponent, 'getDeletedNotes');
       noteListComponent.filter = 'trash';
-      noteListComponent.getDeletedNotes();
+      noteListComponent.retrieveNotes();
       expect(spy).to.have.been.called.once;
     });
   });
